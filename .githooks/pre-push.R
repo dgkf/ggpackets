bf08e077f0c3176c0977082e84b531e0b8988374
dgkf <18220321+dgkf@users.noreply.github.com>
2020-11-06 12:04:10 -0800

    removing heavy doc files

--- .githooks/pre-push.R
+++ .githooks/pre-push.R
@@ -1,7 +1,7 @@
 # run package tests
 test_results <- devtools::test()
 
-if (any(as.data.frame(test_results$failed))) {
+if (any(as.data.frame(test_results)$failed)) {
   message("\n\nErrors encountered while running tests. Push anyways? [y/N] \n")
   resp <- readline(prompt = "")
   if (toupper(resp) != "Y") quit(status = 1L)
